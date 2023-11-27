96d5db1ae182fef9a632b2e3cbe649c9fcccbbb3
Devanshi91m <141355756+Devanshi91m@users.noreply.github.com>
2023-11-27 22:18:52 +0530

    Update Project1.js

--- Project1.js
+++ Project1.js
@@ -1,5 +1,5 @@
 // variables 
-let btn = document.querySelector('new-quote');
+let btn = document.querySelector('#new-quote');
 let quote = document.querySelector('.quote');
 let person = document.querySelector('.person');
 
